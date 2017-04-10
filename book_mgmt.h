2a864cb32738f916b0252fff4ff3081d6b64efa3
HankyWang <HankyWang@users.noreply.github.com>
2017-04-10 16:32:32 +0800

    Update book_mgmt.h

--- book_mgmt.h
+++ book_mgmt.h
@@ -10,7 +10,7 @@ class Book;
 class Book{
 private:
     QDomElement self;
-    QString title,author,press,desp,id,category;
+    QString title,author,press,desp,id,category,loc;
     int amount,total,refer_count,bor_count;
 public:
     int order;
@@ -26,7 +26,7 @@ public:
     void IncIntByTag(QString tag);
     void DecIntByTag(QString tag);
     void display();
-    Book(QString &t,QString &a,QString &p,QString &d,QString &i,QString &c,int am,int to,int rc,int bc);
+    Book(QString &t,QString &a,QString &p,QString &d,QString &i,QString &c,int am,int to,int rc,int bc,QString & lc);
 };
 
 std::vector<Book> * getXml();//Get Xml from file to vector <Book>
@@ -34,9 +34,6 @@ void saveXml();//save Xml from file to vector <Book>
 void update();
 void add_newbook(Book &);
 QString convert2id();
-bool match(Book & b,QRegExp & rx,int mode);
-std::vector<Book *> search(QString kw,int mode);
-
 
 
 #endif // BOOK_MGMT_H
